ad2a7d5b1b00c653d90596dad6cd384b5afdee0b
Antonio Roman <antoniofroman@gmail.com>
2018-09-28 08:06:25 -0400

    Began development. Created class arrayTools and the driver main class for testing. Implemented a method to print arrays.

---
+++ src/Driver.java
@@ -0,0 +1,10 @@
+public class Driver {
+    public static void main(String[] args) {
+        arrayTools arrTool = new arrayTools();
+        String[] names = {"Joshua", "John", "Jose", "Ivan", "Edgard"};
+        System.out.println("Array Tools");
+
+        System.out.println("Names:");
+        arrTool.printStringArray(names);
+    }
+}
---
+++ src/arrayTools.java
@@ -0,0 +1,8 @@
+
+public class arrayTools {
+    public void printStringArray(String[] array) {
+        for(String enteredString : array) {
+            System.out.println(enteredString);
+        }
+    }
+}
